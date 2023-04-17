ad31649071659f1203a0f40a0712ddcb6d487708
ANGEL <atabakov99@abv.bg>
2023-04-18 01:09:47 +0300

    useState & event handlers

---
+++ React Components - Basic Idea/Demo-React/src/components/MovieList.js
@@ -0,0 +1,31 @@
+import Movie from "./Movie"
+
+
+
+export default function MovieList(props) {
+    return (
+        <div>
+         <h1>Movie List</h1>
+    <Movie
+        title={props.movies[0].title}
+        year={props.movies[0].year}
+        cast = {props.movies[0].cast}
+        >
+     </Movie>
+
+     <Movie
+        title={props.movies[1].title}
+        year={props.movies[1].year}
+        cast = {props.movies[1].cast}
+        >
+     </Movie>
+
+     <Movie
+        title={props.movies[2].title}
+        year={props.movies[2].year}
+        cast = {props.movies[2].cast}
+        >
+     </Movie>
+     </div>)
+     
+}
