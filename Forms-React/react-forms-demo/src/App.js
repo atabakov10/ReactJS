7737b2b29108bbbd7e6958c3fe8b27c3cbbaed0e
ANGEL <atabakov99@abv.bg>
2023-05-05 19:31:03 +0300

    React-Forms

--- Forms-React/react-forms-demo/src/App.js
+++ Forms-React/react-forms-demo/src/App.js
@@ -1,30 +1,91 @@
 import './App.css';
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-    const [username, setUsername] = useState('Pesho')
+    const [hobbies, setHobbies] = useState({});
 
-    useEffect(() => {
-        setTimeout(() => {
-            setUsername('Gosho');
-        }, 3000)
-    },[]);
+    const [values, setValues] = useState({
+        username: 'Pesho',
+        creditCard: '',
+        occupation: 'engineering',
+        gender: 'male',
+        bio: '',
+        age: '',
+    });  
 
-    const onUserNameChange = (e) => {
-        setUsername(e.target.value);
-    };
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+    }
+
+    const onChangeHandler = (e) => {
+        setValues(state => ({...state, [e.target.name]: e.target.value}));
+    }
+
+    const onHobbiesChange = (e) => {
+        setHobbies(state => ({ ...state, [e.target.value]: e.target.checked}))
+    }
+
 
     return (
         <div className="App">
             <header className="App-header">
-                <form>
+                <form onSubmit={onSubmitHandler}> 
                     <div>
                         <label htmlFor="username">Username</label>
-                        <input type="text" name="username" id="username" value={username} onChange={onUserNameChange} />
+                        <input type="text" name="username" id="username" value={values.username} onChange={onChangeHandler} />
                     </div>
 
                     <div>
-                        <input type="submit" value="Send"/>
+                        <label htmlFor="age">Age</label>
+                        <input type="number" name="age" id="age" value={values.age ?? ""} onChange={onChangeHandler} />
+                    </div>
+
+                    {
+                        Number(values.age) >= 18 && (
+
+                            <div>
+                                <label htmlFor="credit-card">Credit Card</label>
+                                <input type="text" name="creditCard" id="credit-card" value={values.creditCard} onChange={onChangeHandler} />
+                            </div>
+                        )}
+
+                    <div>
+                        <label htmlFor="occupation">Occupation</label>
+                        <select name="occupation" id="occupation" value={values.occupation} onChange={onChangeHandler}>
+                            <option value="it">IT</option>
+                            <option value="engineering">Engineering</option>
+                            <option value="medicine">Medicine</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="male">Male</label>
+                        <input type="radio" name='gender' id='male' value="male" onChange={onChangeHandler} checked={values.gender === 'male'} />
+                        <label htmlFor="female">Female</label>
+                        <input type="radio" name='gender' id='female' value="female" onChange={onChangeHandler} checked={values.gender === 'female'} />
+
+                    </div>
+
+                    <div>
+                        <label htmlFor="bio">Bio</label>
+                        <textarea name="bio" id="bio" cols="15" rows="5" value={values.bio} onChange={onChangeHandler}></textarea>
+                    </div>
+
+                    <div>
+                        <label htmlFor="hiking">Hiking</label>
+                        <input type="checkbox" name='hobbies' id='hiking' value="hiking" onChange={onHobbiesChange} checked={hobbies["hiking"] || false} />
+                        <label htmlFor="reading">Reading</label>
+                        <input type="checkbox" name='hobbies' id='reading' value="reading" onChange={onHobbiesChange} checked={hobbies["reading"] || false} />
+                        <label htmlFor="sports">Sports</label>
+                        <input type="checkbox" name='hobbies' id='sports' value="sports" onChange={onHobbiesChange} checked={hobbies["sports"] || false} />
+                        <label htmlFor="coding">Coding</label>
+                        <input type="checkbox" name='hobbies' id='coding' value="coding" onChange={onHobbiesChange} checked={hobbies["coding"] || false} />
+                        <label htmlFor="gaming">Gaming</label>
+                        <input type="checkbox" name='hobbies' id='gaming' value="gaming" onChange={onHobbiesChange} checked={hobbies["gaming"] || false} />
+                    </div>
+
+                    <div>
+                        <input type="submit" value="Send" />
                     </div>
                 </form>
             </header>
