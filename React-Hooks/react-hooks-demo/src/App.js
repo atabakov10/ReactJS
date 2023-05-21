f03415eb1ee3284ee74c00ff0af44b9bf8433ebe
ANGEL <atabakov99@abv.bg>
2023-05-21 15:56:06 +0300

    React-Hooks-Demo

--- React-Hooks/react-hooks-demo/src/App.js
+++ React-Hooks/react-hooks-demo/src/App.js
@@ -10,17 +10,23 @@ function App() {
 
     useEffect(() => {
         fetch(baseUrl)
-        .then(res => res.json())
-        .then(data => {
-            setTodos(Object.values(data));
-        })
+            .then(res => res.json())
+            .then(data => {
+                setTodos(Object.values(data));
+            });
     }, []);
-    
+
+    const onTodoDeleteClick = async (todoId) => {
+        await fetch(`${baseUrl}/${todoId}`, { method: 'DELETE' });
+
+        setTodos(state => state.filter(x => x._id !== todoId));
+    }
+
     return (
         <div>
             <Header />
-            
-            <TodoList todos={todos}/>
+
+            <TodoList todos={todos} baseUrl={baseUrl} setTodos={setTodos} onTodoDeleteClick={onTodoDeleteClick} />
         </div>
     );
 }
