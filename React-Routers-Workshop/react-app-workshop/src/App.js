120aa33912bd9cfcbdcffd9a2be52e5341a603d2
ANGEL <atabakov99@abv.bg>
2023-05-22 19:12:25 +0300

    React Authentication

--- React-Routers-Workshop/react-app-workshop/src/App.js
+++ React-Routers-Workshop/react-app-workshop/src/App.js
@@ -10,6 +10,8 @@ import { Route, Routes, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react";
 import * as gameService from "./services/gameService";
 import AuthContext from "./contexts/AuthContext";
+import * as authService from "./services/authService";
+import { Logout } from "./components/Logout/Logout";
 
 function App() {
     const navigate = useNavigate();
@@ -32,18 +34,62 @@ function App() {
     };
 
     const onLoginSubmit = async (data) => {
-        console.log(data);
+
+        try {
+            const result = await authService.login(data);
+
+            setAuth(result);
+
+            navigate('/catalogue');
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const onRegisterSubmit = async (values) => {
+        const { confirmPassword, ...registerData } = values;
+
+        if (confirmPassword !== registerData.password) {
+            return;
+        }
+
+        try {
+            const result = await authService.register(registerData);
+
+            setAuth(result);
+
+            navigate('/catalogue');
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const onLogout = async () => {
+        await authService.logout();
+
+        setAuth({});
+    }
+
+    const context = {
+        onLoginSubmit,
+        onRegisterSubmit,
+        onLogout,
+        userId: auth._id,
+        token: auth.accessToken,
+        userEmail: auth.email,
+        isAuthenticated: !!auth.accessToken,
     }
 
     return (
-        <AuthContext.Provider value={{onLoginSubmit}}>
+        <AuthContext.Provider value={context}>
             <div id="box">
                 <Header />
-
                 <main id="main-content">
+
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/login" element={<Login />} />
+                        <Route path="/logout" element={<Logout />} />
                         <Route path="/register" element={<Register />} />
                         <Route path="/create-game" element={<Create onCreateGameSubmit={onCreateGameSubmit} />} />
                         {/* <Route path="/edit:gameId" element={<Edit />}/> */}
---
+++ React-Routers-Workshop/react-app-workshop/src/components/Logout/Logout.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react'
+import AuthContext from '../../contexts/AuthContext'
+import { Navigate } from 'react-router-dom'
+
+export const Logout = () => {
+    const { onLogout } = useContext(AuthContext);
+
+    onLogout();
+
+    return <Navigate to="/" />
+
+}
