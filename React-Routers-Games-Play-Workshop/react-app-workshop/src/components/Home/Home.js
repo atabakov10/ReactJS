d37cb3ed453f24d452cb52a4d3e0e769a9e62980
ANGEL <atabakov99@abv.bg>
2023-05-26 01:25:04 +0300

    Showing latest games created on home page

--- React-Routers-Games-Play-Workshop/react-app-workshop/src/components/Home/Home.js
+++ React-Routers-Games-Play-Workshop/react-app-workshop/src/components/Home/Home.js
@@ -13,10 +13,10 @@ export const Home = ({
             <img src="./images/four_slider_img01.png" alt="hero" />
 
             <div id="home-page">
-                <h1>Latest Games</h1>
+                {games.length > 0 ? (<h1>Latest Games</h1>) : (<p className="no-articles">No games yet</p>)}
 
-                {games.length > 0 ? games.map(game => <HomeGames key={game._id} {...game}/>) : (<p className="no-articles">No games yet</p>)}
-                
+                {games.length > 0 && games.map(game => <HomeGames key={game._id} {...game} />)}
+
             </div>
         </section>
     );
